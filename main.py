0f428eb9320469ca2528d9ca510280750f1861ae
lezniakb <251574@edu.p.lodz.pl>
2026-03-07 18:40:25 +0100

    added prepare_data() function

--- main.py
+++ main.py
@@ -4,9 +4,29 @@ app = Flask("__name__")
 
 @app.route("/")
 def index():
-    data = api.process_data()
-    carsNum = len(data)
+    params = prepare_data()
     return render_template(
         "index.html",
-        id=data[0]["id"],
-        carsNum=carsNum)
+        id=params["id"],
+        carsNum=params["carsNum"])
+
+
+def prepare_data():
+    data = api.process_data()
+
+    if data is None:
+        params = {
+            "id": None,
+            "carsNum": None
+        }
+        return params
+
+    id = data[0]["id"]
+    carsNum = len(data)
+
+    params = {
+        "id": id,
+        "carsNum": carsNum
+    }
+
+    return params
